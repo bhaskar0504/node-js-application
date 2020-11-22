3ca01c86048700e623c4f357aad92d4129199287
Bhaskar Reddy L <bhaskar0504@gmail.com>
2020-11-22 06:38:47 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# nodejs-app-mss
+
+To start this application first you can get this repo code using below url
+
+git clone https://github.com/bhaskar0504/node-js-application.git
+
+cd nodejs-app-mss
+npm install
+
+node app.js 
+
+(OR) 
+
+npm start
+
+To Execute the SonarQube Repor, execute the below command.
+npm run sonar
+
+(OR) 
+
+node sonar-project.js
---
+++ sonar-project.js
@@ -0,0 +1,16 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://SonarQubeServerIpAddress:PortNumber/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsApp',
+        'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+        'sonar.sourceEncoding':'UTF-8',
+        'sonar.sources': '.',
+	    //'sonar.tests': 'specs',
+        //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
